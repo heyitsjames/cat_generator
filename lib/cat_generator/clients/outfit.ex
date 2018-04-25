2f12aae5a4dd6a3dc8198a02fae47e6ee26bf603
James Rasmussen <james@podium.co>
2018-04-25 15:49:27 -0600

    add adornment server

--- lib/cat_generator/clients/outfit.ex
+++ lib/cat_generator/clients/outfit.ex
@@ -2,7 +2,7 @@ defmodule CatGenerator.Clients.Outfit do
   use HTTPoison.Base
 
   def process_url(gender) do
-    "http://localhost:9001/?gender=#{gender}"
+    "https://adorn-thy-cat.herokuapp.com/?gender=#{gender}"
   end
 
   def process_response_body(body), do: body
